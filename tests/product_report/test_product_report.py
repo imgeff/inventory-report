e86f8293146cd297bbe8b42fe731f51359eeb9a3
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-08-12 15:45:48 -0300

    :white_check_mark: test(unit): Testa método __repr__ da classe Product
    verifica se sua representação é uma 'string' esperada

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,40 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+from tests.data_fakes.product import product_fake
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    (
+        id_do_produto,
+        produto,
+        fabricacao,
+        empresa,
+        validade,
+        serie,
+        armazenamento
+    ) = (
+        product_fake["id"],
+        product_fake["nome_do_produto"],
+        product_fake["data_de_fabricacao"],
+        product_fake["nome_da_empresa"],
+        product_fake["data_de_validade"],
+        product_fake["numero_de_serie"],
+        product_fake["instrucoes_de_armazenamento"]
+    )
+
+    product = Product(
+        id_do_produto,
+        produto,
+        empresa,
+        fabricacao,
+        validade,
+        serie,
+        armazenamento
+    )
+
+    assert str(product) == (
+            f"O produto {produto}"
+            f" fabricado em {fabricacao}"
+            f" por {empresa} com validade"
+            f" até {validade}"
+            f" precisa ser armazenado {armazenamento}."
+        )
