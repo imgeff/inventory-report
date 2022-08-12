a0057041fefbe51b90db0d00a4350dcfb1435e68
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-08-12 15:22:18 -0300

    :recycle: refactor: Centraliza dados fakes de produtos
    para poderem ser reaproveitados em testes futuros

---
+++ tests/data_fakes/product.py
@@ -0,0 +1,9 @@
+product_fake = {
+    "id": 1,
+    "nome_do_produto": "farinha",
+    "nome_da_empresa": "Farinini",
+    "data_de_fabricacao": "01-05-2021",
+    "data_de_validade": "02-06-2023",
+    "numero_de_serie": "1234567890",
+    "instrucoes_de_armazenamento": "ao abrigo de luz"
+}
--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,30 +1,22 @@
 from inventory_report.inventory.product import Product
+from tests.data_fakes.product import product_fake
 
 
 def test_cria_produto():
-    dataTest = {
-        "id": 1,
-        "nome_do_produto": "produto de teste",
-        "nome_da_empresa": "empresa de teste",
-        "data_de_fabricacao": "12-08-2022",
-        "data_de_validade": "22-08-2022",
-        "numero_de_serie": "1234567890",
-        "instrucoes_de_armazenamento": "instruções de teste"
-    }
     product = Product(
-        dataTest["id"],
-        dataTest["nome_do_produto"],
-        dataTest["nome_da_empresa"],
-        dataTest["data_de_fabricacao"],
-        dataTest["data_de_validade"],
-        dataTest["numero_de_serie"],
-        dataTest["instrucoes_de_armazenamento"]
+        product_fake["id"],
+        product_fake["nome_do_produto"],
+        product_fake["nome_da_empresa"],
+        product_fake["data_de_fabricacao"],
+        product_fake["data_de_validade"],
+        product_fake["numero_de_serie"],
+        product_fake["instrucoes_de_armazenamento"]
     )
-    assert product.id == dataTest["id"]
-    assert product.nome_do_produto == dataTest["nome_do_produto"]
-    assert product.nome_da_empresa == dataTest["nome_da_empresa"]
-    assert product.data_de_fabricacao == dataTest["data_de_fabricacao"]
-    assert product.data_de_validade == dataTest["data_de_validade"]
-    assert product.numero_de_serie == dataTest["numero_de_serie"]
+    assert product.id == product_fake["id"]
+    assert product.nome_do_produto == product_fake["nome_do_produto"]
+    assert product.nome_da_empresa == product_fake["nome_da_empresa"]
+    assert product.data_de_fabricacao == product_fake["data_de_fabricacao"]
+    assert product.data_de_validade == product_fake["data_de_validade"]
+    assert product.numero_de_serie == product_fake["numero_de_serie"]
     assert product.instrucoes_de_armazenamento == (
-        dataTest["instrucoes_de_armazenamento"])
+        product_fake["instrucoes_de_armazenamento"])
