6e3061f0e0832b127627f9290de50bd0a13d6070
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-08-13 19:00:29 -0300

    :recycle: refactor: Redefine responsabilidades da classe Inventory
    utiliza classes de leituras de arquivos para diminuição de responsabilidades

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,8 +1,8 @@
-import csv
-import json
-import xml.etree.ElementTree as ET
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
 
 
 class Inventory:
@@ -13,54 +13,12 @@ class Inventory:
         return CompleteReport.generate(data)
 
     @staticmethod
-    def convert_dict_reader_to_list(dict_reader):
-        data = []
-        for row in dict_reader:
-            data.append(row)
-        return data
-
-    @staticmethod
-    def read_csv(path):
-        with open(path, encoding="utf-8") as file:
-            dict_reader = csv.DictReader(file)
-            data = Inventory.convert_dict_reader_to_list(dict_reader)
-        return data
-
-    @staticmethod
-    def read_json(path):
-        with open(path, encoding="utf-8") as file:
-            data = json.load(file)
-        return data
-
-    @staticmethod
-    def read_xml(path):
-        tree = ET.parse(path)
-        root = tree.getroot()
-        data = [
-            {
-                "id": child[0].text,
-                "nome_do_produto": child[1].text,
-                "nome_da_empresa": child[2].text,
-                "data_de_fabricacao": child[3].text,
-                "data_de_validade": child[4].text,
-                "numero_de_serie": child[5].text,
-                "instrucoes_de_armazenamento": child[6].text
-            } for child in root
-        ]
-        
-        return data
-
-    @staticmethod
     def import_data(path, type_report):
         data = ""
         if path.endswith(".json"):
-            data = Inventory.read_json(path)
+            data = JsonImporter.import_data(path)
         elif path.endswith(".csv"):
-            data = Inventory.read_csv(path)
+            data = CsvImporter.import_data(path)
         else:
-            data = Inventory.read_xml(path)
+            data = XmlImporter.import_data(path)
         return Inventory.handle_type_report(data, type_report)
-
-
-# if __name__ == "__main__":
-#     Inventory.read_xml('inventory_report/data/inventory.xml')
