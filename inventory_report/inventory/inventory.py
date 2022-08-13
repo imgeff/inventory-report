338cc51fb50be7a8f7cf0296f9a5a2abf7b63acc
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-08-13 16:17:57 -0300

    :sparkles: feat: flexibiliza método import_data de Inventory para leitura de json

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -18,8 +19,23 @@ class Inventory:
         return data
 
     @staticmethod
-    def import_data(caminho, type_report):
+    def read_csv(caminho):
         with open(caminho, encoding="utf-8") as file:
             dict_reader = csv.DictReader(file)
             data = Inventory.convert_dict_reader_to_list(dict_reader)
-            return Inventory.handle_type_report(data, type_report)
+        return data
+
+    @staticmethod
+    def read_json(caminho):
+        with open(caminho, encoding="utf-8") as file:
+            data = json.load(file)
+        return data
+
+    @staticmethod
+    def import_data(caminho, type_report):
+        data = ""
+        if caminho.endswith(".json"):
+            data = Inventory.read_json(caminho)
+        else:
+            data = Inventory.read_csv(caminho)
+        return Inventory.handle_type_report(data, type_report)
