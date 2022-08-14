af2ffaadddd648f3701cd680369c3fef01d81f03
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-08-13 23:57:34 -0300

    :white_check_mark: test(unit): Implementa teste para método generate de ColoredReport

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,16 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
+from tests.report_decorator.data_tests import (
+    inventory,
+    green_phrases,
+    red_phrases, blue_phrases
+)
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+
+    colored_report = ColoredReport(SimpleReport)
+    report = colored_report.generate(inventory)
+    for list_phrases in green_phrases, red_phrases, blue_phrases:
+        for phrase in list_phrases:
+            assert phrase in report
